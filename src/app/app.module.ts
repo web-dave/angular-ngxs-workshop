03f094dd7b207b3c88e911674d6233b03816bcac
web-dave <dave@webdave.de>
2023-04-26 08:42:34 +0200

    Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
+
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +21,14 @@ import { CoreModule } from './core/core.module';
     ReactiveFormsModule,
     HttpClientModule,
     CoreModule,
+    NgxsModule.forRoot([], {
+      developmentMode: isDevMode(), // Dev Features wie Object.freeze. Wird nur bei der entwicklung benötigt.
+      selectorOptions: {
+        injectContainerState: false, // Jeder Selector checkt den State Container bei jeder Änderung. Nihct gut! Daher abgeschaltet. ab V4 Standardwert=false
+        suppressErrors: false, // Fehler verschlucken?  ab V4 standardwert=false
+      },
+    }),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
