e7b607a503a305d86670acd495e2ebcb8293608d
Dave <dave@webdave.de>
2023-05-27 09:08:18 +0200

    selects

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -7,15 +7,23 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { NgxsModule } from '@ngxs/store';
 import { BooksRoutingModule } from './books-routing.module';
 import { BooksComponent } from './books.component';
 import { BookCardComponent } from './components/book-card/book-card.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookDetailComponent } from './components/book-detail/book-detail.component';
 import { NewBookComponent } from './components/new-book/new-book.component';
+import { BooksState } from './state/book.state';
 
 @NgModule({
-  declarations: [BooksComponent, BookCardComponent, BookListComponent, BookDetailComponent, NewBookComponent],
+  declarations: [
+    BooksComponent,
+    BookCardComponent,
+    BookListComponent,
+    BookDetailComponent,
+    NewBookComponent,
+  ],
   imports: [
     CommonModule,
     BooksRoutingModule,
@@ -27,6 +35,7 @@ import { NewBookComponent } from './components/new-book/new-book.component';
     MatInputModule,
     MatButtonToggleModule,
     MatSnackBarModule,
+    NgxsModule.forFeature([BooksState]),
   ],
 })
 export class BooksModule {}
--- src/app/books/components/book-detail/book-detail.component.ts
+++ src/app/books/components/book-detail/book-detail.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { EMPTY, Observable } from 'rxjs';
+import { Store } from '@ngxs/store';
+import { EMPTY, Observable, of } from 'rxjs';
 import { Book } from '../../models/book';
+import { ActivatedRoute } from '@angular/router';
+import { BooksState } from '../../state/book.state';
+import { filter, map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'ws-book-detail',
@@ -8,9 +12,16 @@ import { Book } from '../../models/book';
   styleUrls: ['./book-detail.component.scss'],
 })
 export class BookDetailComponent implements OnInit {
-  book$: Observable<Book> = EMPTY;
+  book$: Observable<Book>;
 
-  constructor() {}
+  // foo$ = of(1).pipe(map(() => of(2)));
+
+  constructor(private route: ActivatedRoute, private store: Store) {
+    this.book$ = this.route.params.pipe(
+      switchMap((params) => this.store.select(BooksState.entity(params.isbn))),
+      filter((bookOrUndefined): bookOrUndefined is Book => !!bookOrUndefined)
+    );
+  }
 
   ngOnInit(): void {}
 }
