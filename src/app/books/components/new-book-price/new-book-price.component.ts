3618defeb9a7bb3618307c23d9a10a384d356568
Dave <dave@webdave.de>
2023-05-27 14:29:15 +0200

    price step

--- src/app/books/components/new-book-price/new-book-price.component.ts
+++ src/app/books/components/new-book-price/new-book-price.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Select, Store } from '@ngxs/store';
+import {
+  NewBookActions,
+  NewBookState,
+  NewBookStep,
+} from '../../state/new-book.state';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'ws-new-book-price',
   templateUrl: './new-book-price.component.html',
-  styleUrls: ['./new-book-price.component.scss']
+  styleUrls: ['./new-book-price.component.scss'],
 })
 export class NewBookPriceComponent implements OnInit {
+  minPrice$ = this.store
+    .select(NewBookState.numpages)
+    .pipe(map((numPages) => (numPages >= 99 ? 10 : 0)));
 
-  constructor() { }
+  priceForm$ = this.minPrice$.pipe(
+    map(
+      (minAmount) =>
+        new FormGroup({
+          price: new FormControl(0, [
+            Validators.required,
+            Validators.min(minAmount),
+          ]),
+        })
+    )
+  );
 
-  ngOnInit(): void {
-  }
+  constructor(private store: Store) {}
 
+  ngOnInit(): void {}
 }
