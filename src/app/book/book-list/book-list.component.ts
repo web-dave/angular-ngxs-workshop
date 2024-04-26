2209458d0ab2ca1d1d04ba016660b13dc1014fb1
Dave <dave@webdave.de>
2024-04-26 14:15:35 +0200

    select

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -1,21 +1,19 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { BookApiService } from '../book-api.service';
 import { Book } from '../models';
 import { BookCardComponent } from '../book-card/book-card.component';
-import { AsyncPipe, NgFor } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
+import { Select } from '@ngxs/store';
+import { BookCollectionState } from '../state/Book-collection.state';
 
 @Component({
   selector: 'ws-book-list',
   styleUrls: ['./book-list.component.scss'],
   templateUrl: 'book-list.component.html',
   standalone: true,
-  imports: [NgFor, BookCardComponent, AsyncPipe]
+  imports: [BookCardComponent, AsyncPipe]
 })
 export class BookListComponent {
-  protected books$: Observable<Book[]>;
-
-  constructor(private readonly bookService: BookApiService) {
-    this.books$ = this.bookService.getAll();
-  }
+  @Select(BookCollectionState.entities)
+  protected books$!: Observable<Book[]>;
 }
--- src/app/book/state/Book-collection.state.ts
+++ src/app/book/state/Book-collection.state.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Action, State, StateContext } from '@ngxs/store';
+import { Action, Selector, State, StateContext, createSelector } from '@ngxs/store';
 import { BookCollectionStateModel, books } from './book-collection.model';
 import { BookLoadAll } from './book-collection.actions';
 
@@ -14,5 +14,16 @@ export class BookCollectionState {
   @Action(BookLoadAll)
   booksLoadAll(ctx: StateContext<BookCollectionStateModel>, action: BookLoadAll) {
     ctx.setState(state => ({ ...state, entities: books }));
+  }
+
+  @Selector()
+  static entities(state: BookCollectionStateModel) {
+    return state.entities;
+  }
+
+  static entity(isbn: string) {
+    return createSelector([BookCollectionState], (state: BookCollectionStateModel) => {
+      return state.entities.find(entity => entity.isbn === isbn);
+    });
   }
 }
