1419b1c72b233bab44061513bff76a4b0b135c03
Dave <dave@webdave.de>
2024-04-26 15:12:09 +0200

    init renamed

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Store } from '@ngxs/store';
 import { BookLoadAll } from './state/book-collection.actions';
@@ -12,7 +12,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class BookComponent {
   snackBar = inject(MatSnackBar);
-  store = inject(Store)
+  init = inject(Store)
     .dispatch(new BookLoadAll())
     .subscribe({
       next: () => this.snackBar.open('Yay, Booooooks', 'thx')
