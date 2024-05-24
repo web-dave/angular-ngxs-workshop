555b09a6aad9c41baa478838693ddd3a0da12e1e
Dave <dave@webdave.de>
2024-05-24 14:37:42 +0200

    action

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { provideStore } from '@ngxs/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideAnimations(), provideRouter(routes, withComponentInputBinding())]
+  providers: [
+    provideHttpClient(),
+    provideAnimations(),
+    provideStore([], withNgxsReduxDevtoolsPlugin({ disabled: !isDevMode() })),
+    provideRouter(routes, withComponentInputBinding())
+  ]
 };
