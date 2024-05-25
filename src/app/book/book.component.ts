7c9b80b0c86723eaa6f9d83f53e5566fbcf13aff
Dave <dave@webdave.de>
2024-05-25 09:43:08 +0200

    async Action

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Store } from '@ngxs/store';
 import { BookLoadAll } from './state/book-collection.actions';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'ws-book',
@@ -10,8 +11,12 @@ import { BookLoadAll } from './state/book-collection.actions';
   imports: [RouterOutlet]
 })
 export class BookComponent implements OnInit {
+  snackBar = inject(MatSnackBar);
   store = inject(Store);
   ngOnInit(): void {
-    this.store.dispatch(new BookLoadAll());
+    this.store.dispatch(new BookLoadAll()).subscribe({
+      next: () => this.snackBar.open('YAY', 'THX', { duration: 3000 }),
+      complete: () => console.log('Done!')
+    });
   }
 }
