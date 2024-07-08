2f23df086600c7f48e3931cd8cbb82b6d145000d
Dave <dave@webdave.de>
2024-07-08 10:23:34 +0200

    Response

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, Injectable, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Store } from '@ngxs/store';
+import { Actions, Store, ofActionSuccessful } from '@ngxs/store';
 import { BookLoadAll } from './state/book-collection.actions';
 
 @Component({
@@ -11,7 +11,9 @@ import { BookLoadAll } from './state/book-collection.actions';
 })
 export class BookComponent implements OnInit {
   store = inject(Store);
+  actions$ = inject(Actions);
   ngOnInit(): void {
     this.store.dispatch(new BookLoadAll());
+    this.actions$.pipe(ofActionSuccessful(BookLoadAll)).subscribe(data => console.log(data));
   }
 }
