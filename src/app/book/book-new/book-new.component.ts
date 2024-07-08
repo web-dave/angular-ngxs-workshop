bd95c5915537e8dbc92e7caad8859435be499bf1
Dave <dave@webdave.de>
2024-07-08 14:04:03 +0200

    fix ngIf (remove it)

--- src/app/book/book-new/book-new.component.ts
+++ src/app/book/book-new/book-new.component.ts
@@ -1,20 +1,11 @@
-import { Component, DestroyRef, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
-import { tap } from 'rxjs/operators';
-import { BookApiService } from '../book-api.service';
-import { bookNa } from '../models';
-import { MatButton } from '@angular/material/button';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { AsyncPipe, NgIf } from '@angular/common';
-import { MatInput, MatLabel } from '@angular/material/input';
-import { MatError, MatFormField } from '@angular/material/form-field';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AsyncPipe } from '@angular/common';
 import { Store } from '@ngxs/store';
 import { NewBookState } from '../state/new-book.state';
 import { NewBookStep } from '../state/new-book.model';
 import { NewBookSelectStep } from '../state/new-book.actions';
-import { NgxsFormPlugin, NgxsFormPluginModule } from '@ngxs/form-plugin';
 import { BookNewInfoComponent } from './book-new-info/book-new-info.component';
 
 @Component({
@@ -22,48 +13,31 @@ import { BookNewInfoComponent } from './book-new-info/book-new-info.component';
   styleUrls: ['./book-new.component.scss'],
   templateUrl: './book-new.component.html',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatButtonToggleModule,
-    RouterLink,
-    AsyncPipe,
-    NgxsFormPluginModule,
-    BookNewInfoComponent
-  ]
+  imports: [MatButtonToggleModule, RouterLink, AsyncPipe, BookNewInfoComponent]
 })
 export class BookNewComponent {
   store = inject(Store);
   step$ = this.store.select(NewBookState.step);
   NewBookStep = NewBookStep;
-  protected form = this.formBuilder.nonNullable.group({
-    title: ['', [Validators.required]],
-    subtitle: [''],
-    author: ['', [Validators.required]],
-    abstract: [''],
-    isbn: ['', [Validators.required, Validators.minLength(3)]],
-    cover: [''],
-    numPages: [0, [Validators.required, Validators.min(10)]]
-  });
-
   selectStep(nextStep: NewBookStep) {
     this.store.dispatch(new NewBookSelectStep(nextStep));
   }
 
-  constructor(
-    private readonly formBuilder: FormBuilder,
-    private readonly router: Router,
-    private readonly bookService: BookApiService,
-    private readonly destroyRef: DestroyRef
-  ) {}
+  // constructor(
+  //   private readonly formBuilder: FormBuilder,
+  //   private readonly router: Router,
+  //   private readonly bookService: BookApiService,
+  //   private readonly destroyRef: DestroyRef
+  // ) {}
 
-  create() {
-    const book = { ...bookNa(), ...this.form.getRawValue() };
-    this.bookService
-      .create(book)
-      .pipe(
-        takeUntilDestroyed(this.destroyRef),
-        tap(() => this.router.navigateByUrl('/'))
-      )
-      .subscribe();
-  }
+  // create() {
+  // const book = { ...bookNa(), ...this.form.getRawValue() };
+  // this.bookService
+  //   .create(book)
+  //   .pipe(
+  //     takeUntilDestroyed(this.destroyRef),
+  //     tap(() => this.router.navigateByUrl('/'))
+  //   )
+  //   .subscribe();
+  // }
 }
