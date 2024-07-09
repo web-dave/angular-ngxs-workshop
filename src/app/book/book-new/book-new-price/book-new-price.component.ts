e5f960a37058e25ffa8fb2258ea3f4bd5efae43f
Dave <dave@webdave.de>
2024-07-09 15:11:50 +0200

    nonnullable Forms

--- src/app/book/book-new/book-new-price/book-new-price.component.ts
+++ src/app/book/book-new/book-new-price/book-new-price.component.ts
@@ -1,14 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { NewBookState } from '../../state/new-book.state';
 import { map } from 'rxjs';
-import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AsyncPipe } from '@angular/common';
 import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { NgxsFormPluginModule } from '@ngxs/form-plugin';
-import { NewBookSubmitStep } from '../../state/new-book.actions';
+import { NewBookSetPages, NewBookSubmitStep } from '../../state/new-book.actions';
 import { NewBookStep } from '../../state/new-book.model';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'ws-book-new-price',
@@ -17,16 +18,46 @@ import { NewBookStep } from '../../state/new-book.model';
   templateUrl: './book-new-price.component.html',
   styleUrl: './book-new-price.component.scss'
 })
-export class BookNewPriceComponent {
+export class BookNewPriceComponent implements OnInit {
   store = inject(Store)
   formBuilder = inject(NonNullableFormBuilder)
+  dRef = inject(DestroyRef)
+  numPages = 123
+  minPrice = 0
 
-  form$ = this.store.select(NewBookState.info).pipe(map(info =>{
-    const minPrice = info.model.numPages >=100? 10:0;
-    return this.formBuilder.group({
-      price: [minPrice,[Validators.required, Validators.min(minPrice)]]
-    })
-  }))
+  form: FormGroup<{price: FormControl<number>}> = this.formBuilder.group({
+    price: [0,[Validators.required]]
+  })
+
+  ngOnInit(): void {
+    this.store.select(NewBookState.info).pipe(
+      map(info =>{
+      const minPrice = info.model.numPages >=100? 10:0;
+      const minValidator = Validators.min(minPrice);
+      const priceCtrl = this.form.get('price') as FormControl<number>;
+      priceCtrl.removeValidators(Validators.min(this.minPrice))
+      this.minPrice = minPrice
+      priceCtrl.addValidators([minValidator]),
+      console.log(priceCtrl)
+      priceCtrl.updateValueAndValidity()
+    }),
+    takeUntilDestroyed(this.dRef)
+  ).subscribe()
+    
+  }
+
+  setPages(){
+this.numPages = this.numPages === 99? 123: 99
+
+this.store.dispatch(new NewBookSetPages(this.numPages))
+  }
+
+  // form$ = this.store.select(NewBookState.info).pipe(map(info =>{
+  //   const minPrice = info.model.numPages >=100? 10:0;
+  //   return this.formBuilder.group({
+  //     price: [minPrice,[Validators.required, Validators.min(minPrice)]]
+  //   })
+  // }))
 
   submit(){
     this.store.dispatch(new NewBookSubmitStep(NewBookStep.price));
