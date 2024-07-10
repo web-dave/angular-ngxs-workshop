3fac57c87060873fbad0f37f66ae8360e7fbf887
Dave <dave@webdave.de>
2024-07-10 12:24:33 +0200

    cypress interaction und soweiter

---
+++ cypress/e2e/books/create.book.cy.ts
@@ -0,0 +1,58 @@
+describe('As a user I want to check if a book can be created.', () => {
+  let randomISBN = 0;
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('mat-card').as('books');
+  });
+  afterEach(() => {
+    cy.request('DELETE', 'http://localhost:4730/books/' + randomISBN);
+  });
+
+  it('should increase the number of books by 1', () => {
+    let countBefore = 0;
+    randomISBN = Math.floor(1000000000000 + Math.random() * 900000);
+    cy.get('@books')
+      .then(books => (countBefore = books.length))
+      // navigate to create form
+
+      .then(() => {
+        cy.get('[routerlink="books/new"]').click();
+
+        cy.get('[formControlName="isbn"]').type('9781787125421').blur();
+        cy.wait(1000);
+        cy.get('mat-error').should('exist');
+        cy.contains('9781787125421 wird schon verwendet!');
+        cy.get('[formControlName="isbn"]').clear();
+        cy.get('[formControlName="isbn"]').type(randomISBN + '');
+        cy.get('mat-error').should('not.exist');
+
+        cy.get('[formControlName="numPages').type(107 + '');
+
+        cy.get('[mat-raised-button=""]').should('be.disabled');
+
+        const fileds = ['title', 'subtitle', 'author', 'abstract', 'cover'];
+        fileds.forEach(field => {
+          cy.get(`[formControlName="${field}"]`).type('test in ' + field);
+        });
+        cy.get('[mat-raised-button=""]').should('not.be.disabled').click();
+
+        cy.get('[mat-raised-button=""]').should('be.disabled');
+
+        cy.get(`[formControlName="price"]`).type('9').blur();
+        cy.get('mat-error').should('exist');
+        cy.contains(' Price is too low (10)');
+        cy.get(`[formControlName="price"]`).clear().type('13').blur();
+        cy.get('mat-error').should('not.exist');
+        cy.screenshot('Form OK');
+        cy.get('[mat-raised-button=""]').should('not.be.disabled').click({ force: true });
+        cy.get('[routerlink="/"]').click();
+        cy.get('@books').should('have.length.greaterThan', countBefore);
+
+        // cy.get('<form-control>').type('value')
+        // ...
+        // submit form
+      });
+    // navigate to books list
+    // assert books length
+  });
+});
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,7 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running')
-  })
-})
+    cy.visit('/');
+    cy.contains('BOOK MONKEY');
+    cy.get('mat-card').first().should('contain.text', 'Web');
+  });
+});
