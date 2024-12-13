69e1ef517a711aca087049d00c498335cdec4e42
Dave <dave@webdave.de>
2024-12-13 16:13:22 +0100

    Add ngxs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngxs/store';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideAnimations(), provideRouter(routes, withComponentInputBinding())]
+  providers: [
+    provideHttpClient(),
+    provideAnimations(),
+    provideRouter(routes, withComponentInputBinding()),
+    provideStore([], withNgxsReduxDevtoolsPlugin({ disabled: !isDevMode() }))
+  ]
 };
