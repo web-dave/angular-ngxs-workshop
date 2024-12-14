b9f36dcbcf73eb9d7d65971d01ceb64f28d75766
Dave <dave@webdave.de>
2024-12-14 09:41:10 +0100

    manage action

--- src/app/book/book.routes.ts
+++ src/app/book/book.routes.ts
@@ -4,11 +4,14 @@ import { BookDetailComponent } from './book-detail/book-detail.component';
 import { BookNewComponent } from './book-new/book-new.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookEditComponent } from './book-edit/book-edit.component';
+import { provideStore } from '@ngxs/store';
+import { BookCollectionState } from './state/book-collection.state';
 
 export const bookRoutes: Routes = [
   {
     path: '',
     component: BookComponent,
+    providers: [provideStore([BookCollectionState])],
     children: [
       {
         path: '',
