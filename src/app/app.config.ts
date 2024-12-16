08e44fab6d0736aac9dac2363d148d6a8623ae51
Dave <dave@webdave.de>
2024-12-16 08:54:23 +0100

    PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,17 +7,17 @@ import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { withNgxsFormPlugin } from '@ngxs/form-plugin';
 import { withNgxsRouterPlugin } from '@ngxs/router-plugin';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideAnimations(),
     provideRouter(routes, withComponentInputBinding()),
-    provideStore(
-      [],
-      withNgxsFormPlugin(),
-      withNgxsRouterPlugin(),
-      withNgxsReduxDevtoolsPlugin({ disabled: !isDevMode() })
-    )
-  ]
+    provideStore([], withNgxsFormPlugin(), withNgxsRouterPlugin(), withNgxsReduxDevtoolsPlugin({ disabled: !isDevMode() })),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
