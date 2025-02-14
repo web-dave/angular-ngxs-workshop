fabda65246409a63485228542e59ab475909b62e
Dave <dave@webdave.de>
2025-02-14 11:54:00 +0100

    init store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideAnimations(), provideRouter(routes, withComponentInputBinding())]
+  providers: [
+    provideHttpClient(),
+    provideAnimations(),
+    provideRouter(routes, withComponentInputBinding()),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+  ]
 };
