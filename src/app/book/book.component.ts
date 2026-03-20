7936c17f3dbc1f6674f22a560670209aaef91151
Dave <dave@webdave.de>
2026-03-20 14:10:28 +0100

    action

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { BookLoadAll } from './state/book.actions';
 
 @Component({
   selector: 'ws-book',
@@ -7,4 +9,9 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet]
 })
-export class BookComponent {}
+export class BookComponent implements OnInit {
+  private readonly store = inject(Store);
+  ngOnInit(): void {
+    this.store.dispatch(new BookLoadAll());
+  }
+}
