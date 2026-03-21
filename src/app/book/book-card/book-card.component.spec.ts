1995852ed553d35f9511dda0261df82af75a855e
Dave <dave@webdave.de>
2026-03-21 16:02:45 +0100

    template test

--- src/app/book/book-card/book-card.component.spec.ts
+++ src/app/book/book-card/book-card.component.spec.ts
@@ -1,5 +1,8 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { BookCardComponent } from './book-card.component';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { bookNa } from '../models';
 
 describe('BookCardComponent', () => {
   it('should show n/a', () => {
@@ -8,6 +11,32 @@ describe('BookCardComponent', () => {
       const content = card.content;
       expect(content.title).toBe('n/a');
       expect(content.author).toBe('n/a');
+    });
+  });
+  describe('Template', () => {
+    let fixture: ComponentFixture<BookCardComponent>;
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [BookCardComponent],
+        schemas: [NO_ERRORS_SCHEMA],
+        providers: [provideRouter([])]
+      });
+      fixture = TestBed.createComponent(BookCardComponent);
+      fixture.detectChanges();
+    });
+
+    it('should show Title n/a', () => {
+      const template: HTMLElement = fixture.debugElement.nativeElement;
+      const title = template.querySelector('mat-card-title') as HTMLElement;
+      expect(title.innerText).toContain('n/a');
+    });
+
+    it('should show Title Das Leben ist schön!', () => {
+      fixture.componentRef.setInput('content', { ...bookNa(), title: 'Das Leben ist schön!' });
+      fixture.detectChanges();
+      const template: HTMLElement = fixture.debugElement.nativeElement;
+      const title = template.querySelector('mat-card-title') as HTMLElement;
+      expect(title.innerText).toContain('Das Leben ist schön!');
     });
   });
 });
